6c30a6d6a9c53726493d97133025d7abf3b95a47
Rafał Pyżalski <r.pyzalski@stud.elka.pw.edu.pl>
2025-07-01 22:32:39 +0200

    Fix pre-commit config args format

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+A project that’s never finished, approach n.
+
+Expense management module:
+-loads data from a JSON file,
+-saves new expenses,
+-analyzes budget overruns.
+"""
+
+import json
+import os
+
+DATA_FILE = "data.json"
+
+
+def load_data():
+    """Load expenses data from a JSON file and return it as a list."""
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as json_file:
+        data = json.load(json_file)
+        return data
+
+
+def save_data(data):
+    """Save the expenses data list to a JSON file."""
+    with open(DATA_FILE, "w") as json_file:
+        json.dump(data, json_file, indent=4)
